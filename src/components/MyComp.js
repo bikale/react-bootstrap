cbc9027597ac4d08c59e444cd57eb93506ca7283
“bikale” <bikaleg@gmail.com>
2020-03-31 01:55:47 -0500

    bs-grid

--- src/components/MyComp.js
+++ src/components/MyComp.js
@@ -4,6 +4,7 @@ export default function MyComp() {
   return (
     <div>
       {/* Root div */}
+
       <div className="container">
         <h1 className="page-header">Hello world!</h1>
         <hr className="my-4" />
@@ -71,6 +72,54 @@ export default function MyComp() {
         </ul>
       </div>
 
+      <div className="container">
+        {/* GRIDs */}
+        <h4>GRIDs</h4>
+        <div className="row">
+          <div className="col-md-8 col-sm-8">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              corporis nihil, quam laboriosam quo error provident obcaecati
+              dolore. Soluta numquam fugiat iste officia ipsum, alias modi
+              ratione aliquid mollitia. Nisi!
+            </p>
+          </div>
+          <div className="col-md-4 col-sm-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              corporis nihil, quam laboriosam quo error provident obcaecati
+              dolore. !
+            </p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              corporis nihil, quam laboriosam quo error provident obcaecati
+              dolore. Soluta numquam fugiat iste officia ipsum, alias modi
+              ratione aliquid mollitia. Nisi!
+            </p>
+          </div>
+          <div className="col-md-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              corporis nihil, quam laboriosam quo error provident obcaecati
+              dolore. !
+            </p>
+          </div>
+
+          <div className="col-md-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              corporis nihil, quam laboriosam quo error provident obcaecati
+              dolore. !
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr />
       <div className="container">
         {/* Buttons */}
 
@@ -210,11 +259,37 @@ export default function MyComp() {
         </div>
 
         <hr />
+      </div>
 
-        {/* progress bars */}
+      <div className="container">
+        {/* Badges */}
+        <a href="#" className="btn btn-primary">
+          Inbox <span className="badge badge-light">13</span>{' '}
+        </a>
+        <button className="btn btn-info">
+          Message <span className="badge badge-light">23</span>{' '}
+        </button>
+      </div>
 
-
-        
+      <div className="container">
+        <hr />
+        {/* Images */}
+        <img
+          src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1712f4c1c8a%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1712f4c1c8a%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2273.6328125%22%20y%3D%22104.5%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+          alt="..."
+          className="img-thumbnail"
+        />{' '}
+        <br />
+        <br />
+        <img
+          src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1712f4c1c8a%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1712f4c1c8a%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2273.6328125%22%20y%3D%22104.5%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+          className="rounded float-left"
+        />
+        <img
+          src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1712f4c1c8a%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1712f4c1c8a%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2273.6328125%22%20y%3D%22104.5%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+          className="rounded float-right"
+          alt="..."
+        />
       </div>
 
       <div style={{ height: '400px' }}></div>
