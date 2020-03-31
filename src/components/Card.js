1075d42d84cbfa3403073d3d7d69abf478bb509b
“bikale” <bikaleg@gmail.com>
2020-03-31 16:45:26 -0500

    simple react app with bs4

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import img1 from '../assets/game.jpg';
-import './card-style.css'
+
+import './card-style.css';
 export default function Card(props) {
   return (
     <div>
-      <div className="card text-center">
+      <div className="card text-center shadow">
         <div className="overflow">
-          <img src={img1} alt="" srcset="" className='card-img-top'/>
+          <img src={props.imgsrc} alt="" srcset="" className="card-img-top" />
         </div>
         <div className="card-body text-dark">
-          <h4 className="card-title">Game Player</h4>
+          <h4 className="card-title">{props.title}</h4>
           <p className="card-text text-secondary">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
             deserunt quidem impedit ut cumque eveniet ad praesentium maxime illo
             temporibus.
           </p>
           <a href="#" className="btn btn-outline-success">
-            read more ...
+            Read more ...
           </a>
         </div>
       </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import Crad from './Card';
+import NavBar from './NavBar';
 import Card from './Card';
+import img1 from '../assets/chess.jpeg';
+import img2 from '../assets/alone-console.jpg';
+import img3 from '../assets/game.jpg';
 export default function Cards() {
   return (
-    <div className="container-fluid d-flex justify-content-center">
-      <div className="row">
-        <div className="col-md-4 col-sm-4">
-          <Card />
-        </div>
-        <div className="col-md-4 col-sm-4">
-          <Card />
-        </div>
-        <div className="col-sm-4">
-          <Card />
+    <div>
+      <NavBar />
+      <div className="container-fluid d-flex justify-content-center">
+        <div className="row">
+          <div className="col-md-4 col-sm-4">
+            <Card imgsrc={img1} title="Chess" />
+          </div>
+          <div className="col-md-4 col-sm-4">
+            <Card imgsrc={img2} title="Console" />
+          </div>
+          <div className="col-md-4 col-sm-4">
+            <Card imgsrc={img3} title="Game center" />
+          </div>
         </div>
       </div>
     </div>
